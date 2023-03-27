d14b75601503295b1495b8a69aa01e8a873c0e61
Levi Williams <ljw758@hotmail.com>
2023-03-26 23:54:19 -0500

    Completed Exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TempConverter.CelciusToFarenheit(0);
+            Console.WriteLine(TempConverter.CelciusToFarenheit(0));
+            TempConverter.FarenheitToCelcius(32);
+            Console.WriteLine(TempConverter.FarenheitToCelcius(32));
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FarenheitToCelcius(double x)
+        {
+            return (x - 32) * (5 / 9);
+        }
+
+        public static double CelciusToFarenheit(double x)
+        {
+            return (x * (5 / 9)) + 32;
+        }
+    }
+}
